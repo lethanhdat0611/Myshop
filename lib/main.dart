51569aea37189d6fec92dd5c8e81667f15415082
lethanhdat0611 <113762542+lethanhdat0611@users.noreply.github.com>
2023-02-12 14:48:19 +0700

    Xay dung trang tong quan cac san pham

--- lib/main.dart
+++ lib/main.dart
@@ -31,8 +31,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-      home: SafeArea(
+      home: const SafeArea(
           child: ProductsOverviewScreen(),
+        
         ),
     );
   }
